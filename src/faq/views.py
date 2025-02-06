7179b66c532f39c44d4883625a2a926451ab08b0
YongJun-Lee-98 <leep0415@gmail.com>
2025-02-06 15:59:42 +0900

    Chore: 오타수정

--- src/faq/views.py
+++ src/faq/views.py
@@ -8,8 +8,8 @@ from .serializers import FAQSerializer
 # Create your views here.
 # 예상 CRUD
 @extend_schema_view(
-    list=extend_schema(summary="FAQ 목록 조회", description="등록된 FAQ 모곩을 조회합니다."),
-    # retrieve=extend_schema(summary="FAQ 상세 조회", description="특정 FAQ의 상세정보를 조회")
+    list=extend_schema(summary="FAQ 목록 조회", description="등록된 FAQ 목록을 조회합니다."),
+    retrieve=extend_schema(summary="FAQ 상세 조회", description="특정 FAQ의 상세정보를 조회"),
     create=extend_schema(summary="FAQ 생성", description="새로운 FAQ를 생성합니다."),
     update=extend_schema(summary="FAQ 수정", description="기존 FAQ 정보를 수정합니다."),
     partial_update=extend_schema(summary="FAQ 부분 수정", description="FAQ 일부 정보를 수정합니다."),
