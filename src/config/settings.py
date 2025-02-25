b1b83a7aba82c93de3decdbcfba1291ca38b6626
jungmir <ham4051@gmail.com>
2025-02-26 02:26:20 +0900

    Chore: 일부 settings 옵션을 환경 변수로 제어 할 수 있도록 수정 (#57)

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)y8gs-x9_4k0)9o#_v+i*2s-%_mwkq%%2eqx#eshs8)^&_0#4k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",")
 
 # 외부 라이브러리 (Third-party Apps)
 THIRD_PARTY_APPS = ["rest_framework", "drf_spectacular", "jazzmin"]
