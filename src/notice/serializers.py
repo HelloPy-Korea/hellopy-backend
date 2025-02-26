67e31b4ac0dc8b56cdd2741b7eb7be49f1d702a0
jungmir <ham4051@gmail.com>
2025-02-27 00:09:07 +0900

    Fix: 공지 사항 목록 조회시에 display되지 않는 필드는 포함하지 않도록 수정

--- src/notice/serializers.py
+++ src/notice/serializers.py
@@ -3,7 +3,13 @@ from rest_framework import serializers
 from .models import Notice
 
 
+class NoticeSummarizeListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notice
+        fields = ["id", "title", "is_pinned"]
+
+
 class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
-        fields = ["title", "content", "is_deleted", "is_pinned", "created_at", "updated_at"]
+        fields = ["id", "title", "content", "is_pinned", "created_at", "updated_at"]
