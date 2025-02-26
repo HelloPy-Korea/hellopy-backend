48939accdc0c2e764eed8e561228c1fd0bcdff91
jungmir <ham4051@gmail.com>
2025-02-26 22:45:56 +0900

    Fix: 컬럼에 대한 verbose_name 정의

--- src/notice/models.py
+++ src/notice/models.py
@@ -3,14 +3,19 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 
 class Notice(models.Model):
-    title = models.CharField(max_length=255, verbose_name="제목")
-    content = CKEditor5Field("Content", config_name="extends")
-    is_deleted = models.BooleanField(default=False, verbose_name="삭제 여부")
-    is_pinned = models.BooleanField(verbose_name="고정 여부")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField("제목", max_length=255)
+    content = CKEditor5Field("본문", config_name="extends")
+    is_deleted = models.BooleanField("삭제 여부", default=False)
+    is_pinned = models.BooleanField("상단 고정", default=False)
+    created_at = models.DateTimeField("등록 일시", auto_now_add=True)
+    updated_at = models.DateTimeField("수정 일시", auto_now=True)
+
     class Meta:
         verbose_name = "공지"
         verbose_name_plural = "공지사항"
+
     def __str__(self):
         return self.title
+
+    def __repr__(self):
+        return f"Notice(id={self.id}, title={self.title})"
