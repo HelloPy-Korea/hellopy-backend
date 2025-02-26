cac3b365bc64d9b458e86d36bc1566a8abbf9105
jungmir <ham4051@gmail.com>
2025-02-26 22:47:45 +0900

    Style: 주석 제거 후 display field에 id 추가

--- src/notice/admin.py
+++ src/notice/admin.py
@@ -6,13 +6,13 @@ from .models import Notice
 # Register your models here.
 @admin.register(Notice)
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ("title", "is_pinned", "created_at", "updated_at")
+    list_display = ("id", "title", "is_pinned", "created_at", "updated_at")
     list_filter = ("is_pinned",)
     search_fields = ("title", "content")
     ordering = ("-created_at",)
-    
+
     fieldsets = (
         ("수정 가능 필드", {"fields": ("title", "content", "is_pinned")}),
-        # ("자동 설정 필드", {"fields": ("created_at", "updated_at"), "classes": ("collapse",)}),
+        ("자동 설정 필드", {"fields": ("created_at", "updated_at"), "classes": ("collapse",)}),
     )
     readonly_fields = ("created_at", "updated_at")
