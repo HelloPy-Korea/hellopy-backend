7b647c0efdf56ba7b598f1146a4ed36b4041fa91
jungmir <ham4051@gmail.com>
2025-02-26 22:49:42 +0900

    Style: ruff 적용

--- src/notice/serializers.py
+++ src/notice/serializers.py
@@ -4,7 +4,6 @@ from .models import Notice
 
 
 class NoticeSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Notice
-        fields = ['title', 'content', 'is_deleted', 'is_pinned', 'created_at', 'updated_at']
+        fields = ["title", "content", "is_deleted", "is_pinned", "created_at", "updated_at"]
