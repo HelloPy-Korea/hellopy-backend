cb47db385b8f3eabbc0fabe39a79c1cb6fd85b08
YongJun-Lee-98 <leep0415@gmail.com>
2025-03-05 15:49:08 +0900

    Chore: ruff style

--- src/faq/migrations/0006_alter_faq_options.py
+++ src/faq/migrations/0006_alter_faq_options.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("faq", "0005_alter_faq_is_deleted"),
     ]
--- src/merchandise/migrations/0002_alter_merchandise_image.py
+++ src/merchandise/migrations/0002_alter_merchandise_image.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("merchandise", "0001_initial"),
     ]
