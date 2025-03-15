63ef9f5ec1f25443cc2f6919fadf9da375085116
jungmir <ham4051@gmail.com>
2025-03-15 22:10:01 +0900

    Fix: soft delete 로직 반영

--- src/notice/models.py
+++ src/notice/models.py
@@ -2,11 +2,12 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
 
+from core.mixins.models import SoftDeleteModel
 
-class Notice(models.Model):
+
+class Notice(SoftDeleteModel):
     title = models.CharField("제목", max_length=255)
     content = CKEditor5Field("본문", config_name="extends", null=False, blank=False)
-    is_deleted = models.BooleanField("삭제 여부", default=False)
     is_pinned = models.BooleanField("상단 고정", default=False)
     created_at = models.DateTimeField("등록 일시", auto_now_add=True)
     updated_at = models.DateTimeField("수정 일시", auto_now=True)
